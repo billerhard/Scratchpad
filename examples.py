e5199e66e276139712e77d061a2609d8ba56737c
Bill <bill.erhard@gmail.com>
2023-08-06 01:04:56 -0700

    f strings are ugly, but are the preferred (speedy) method to format strings.

--- examples.py
+++ examples.py
@@ -5,8 +5,8 @@ from discount import calc_discount
 def main():
     """Examples of modules in the scratchpad."""
 
-    print(calc_discount(100, 20))
-    print(calc_discount(135.99, 17))
+    print(f"${calc_discount(100, 20):0.2f}" )
+    print(f"${calc_discount(135.99, 17):0.2f}")
 
 
 if __name__ == "__main__":
