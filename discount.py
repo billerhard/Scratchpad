2ef2d3d51ab76351e22f6e16f41120b870925f79
Bill <bill.erhard@gmail.com>
2023-08-06 00:41:19 -0700

    discount initial commit

---
+++ discount.py
@@ -0,0 +1,9 @@
+"""
+Discount Calc
+Returns a calculated discount
+"""
+
+def calc_discount(price, discount):
+    """returns the discounted price
+    ex.: 100, 20 returns 80"""
+    return price * discount / 100
