0629c0acc0e3fe5bf18305d56cafb06c6d7b93e8
Bill <bill.erhard@gmail.com>
2023-08-06 02:16:35 -0700

    vErBoSe

--- examples.py
+++ examples.py
@@ -1,13 +1,15 @@
 """
 Examples"""
-from discount import calc_discount
+#pylint: disable=unused-import
+from discount import calc_discount 
 
 def main():
     """Examples of modules in the scratchpad."""
 
-    print(f"${calc_discount(100, 20):0.2f}" )
+    print("print(f\"${calc_discount(100, 20):0.2f}\")")
+    print(f"${calc_discount(100, 20):0.2f}")
+    print("print(f\"${calc_discount(135.99, 17):0.2f}\")")
     print(f"${calc_discount(135.99, 17):0.2f}")
-
 
 if __name__ == "__main__":
     main()
