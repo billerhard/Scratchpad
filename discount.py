da19c174ac88e882df6090b4f31e61f5b97fe55f
Bill <bill.erhard@gmail.com>
2023-08-06 00:47:03 -0700

    fixes

--- discount.py
+++ discount.py
@@ -3,7 +3,7 @@ Discount Calc
 Returns a calculated discount
 """
 
-def calc_discount(price, discount):
+def calc_discount(price: float, discount: float) -> float:
     """returns the discounted price
-    ex.: 100, 20 returns 80"""
-    return price * discount / 100
+    ex.: a price of $100.00 with a discount of 20% returns $80.00"""
+    return round(price - price * (discount / 100), 2)
