76889a127bc58d5255924beb703754a7667fad4e
Bill <bill.erhard@gmail.com>
2023-08-06 00:56:27 -0700

    examples and discounts

--- discount.py
+++ discount.py
@@ -7,3 +7,8 @@ def calc_discount(price: float, discount: float) -> float:
     """returns the discounted price
     ex.: a price of $100.00 with a discount of 20% returns $80.00"""
     return round(price - price * (discount / 100), 2)
+
+if __name__ == "__main__":
+    import sys
+    calc_discount(float(sys.argv[1]), float(sys.argv[2]))
+    
---
+++ examples.py
@@ -0,0 +1,13 @@
+"""
+Examples"""
+from discount import calc_discount
+
+def main():
+    """Examples of modules in the scratchpad."""
+
+    print(calc_discount(100, 20))
+    print(calc_discount(135.99, 17))
+
+
+if __name__ == "__main__":
+    main()
