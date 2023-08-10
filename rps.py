4de625cb4378ae44e9e1fd585fc547e20bfe9b8b
Bill <bill.erhard@gmail.com>
2023-08-09 21:52:04 -0700

    oh, that's how it works.

--- rps.py
+++ rps.py
@@ -2,14 +2,46 @@
 Rock Paper Scissors
 Would you like to play a game of Rock, Paper, Scissors?
 """
-from random import randint
+from random import randrange
 
 
 def rock_paper_scissors() -> None:
     """Takes user input, calculates a random rps throw,
     and prints whether the user won or lost."""
-    rps_throw = randint(0, 2)
+    rps_throw = randrange(1, 4)
+    player_throw = player_input()
+    throws = ["rock", "paper", "scissors"]
+    outcome = f"Computer threw {throws[rps_throw-1]}. Player threw {throws[player_throw-1]}."
 
+    if rps_throw == player_throw:
+        print(f"{outcome} Player ties!")
+    elif (rps_throw + 1) == player_throw or (rps_throw == 3 and player_throw == 1):
+        print(f"{outcome} Player wins!")
+    else:
+        print(f"{outcome} Player loses!")
+
+
+def player_input() -> int:
+    """
+    get player input 1, 2 or 3 for rps."""
+    number = 0
+    while number < 1 or number > 3:
+        number = input(
+            "Please enter a number.\n\
+            1 for rock\n\
+            2 for paper.\n\
+            3 for scissors.\n\
+            Your number: "
+        )
+        try:
+            number = int(number)
+            if number < 1 or number > 3:
+                raise ValueError()
+            return number
+        except ValueError:
+            print(f"Number ({number}) input must be an integer 1, 2, or 3.")
+            number = 0
+    return None
 
 
 if __name__ == "__main__":
